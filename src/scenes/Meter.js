62fe062010e5efa9a00463a688b022d94e8ab919
Dan Engel <dengel29@gmail.com>
2019-02-21 09:46:54 +0800

    fill centered inside of empty meter

--- src/scenes/Meter.js
+++ src/scenes/Meter.js
@@ -5,20 +5,22 @@ export class Meter extends Phaser.Scene {
         super({key:"Meter"}) 
     }
     preload() {
-        this.load.image("meter", 'assets/hypemeter_empty.png');
-        this.load.image("fill", 'assets/hypemeter_fill.png');
+        this.load.image("meter", 'assets/hypemeter_empty.png'); //436x30
+        this.fillImage = this.load.image("fill", 'assets/hypemeter_fill.png');
     }
 
     create() {
-        
-        this.meter = this.add.image(400,300,'meter')
+        this.startX = 400
+        this.startY = 300
+        this.meter = this.add.image(this.startX, this.startY,'meter')
         this.fill = this.add.nineslice(
-            400, 300,
-            30, 30,
+            this.startX - 219, this.startY -15,
+            436, 30,
             'fill',
-      [35, 15, 15])
+            10, // the width and height to offset for a corner slice
+            0// (optional) pixels to offset when computing the safe usage area
 
-
+        )
     }
 
     update(){}
